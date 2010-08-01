aefe196111961c8c88b75970fb1d5302fbe32ee2
Chris Trees <christrees@yahoo.com>
2010-08-01 15:33:11 -0500

    initial push of skeleton generated by rails catlog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '93c41c728e827f0cce030c396d4d0e30c635b22aa740dfa4515c92a3e49a8df9eb841b62916498fbc69709f13f1385bd40dc17e37163e27df1023a01e9820321'
